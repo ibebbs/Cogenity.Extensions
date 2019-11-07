6e33ed7ecdb2e14c168edc12aad930a7b7247ffe
ibebbs <github@bebbs.co.uk>
2019-11-07 17:08:19 +0000

    Removed unused namespaces from LoadContext

--- src/Microsoft.Extensions.Hosting.Composition/Module/LoadContext.cs
+++ src/Microsoft.Extensions.Hosting.Composition/Module/LoadContext.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.Loader;
-using System.Text;
 
 namespace Microsoft.Extensions.Hosting.Composition.Module
 {
